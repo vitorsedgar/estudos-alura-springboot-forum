640bf16cffa4527ad83e76f7cce601f1578ed57a
Vitor Edgar da Silva <vitorsedgar@gmail.com>
2020-09-24 23:23:00 -0300

    autoken

---
+++ src/main/java/br/com/edgar/vitor/easforum/config/security/TokenService.java
@@ -0,0 +1,34 @@
+package br.com.edgar.vitor.easforum.config.security;
+
+import br.com.edgar.vitor.easforum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API do Fórum da Alura")
+                .setSubject(String.valueOf(logado.getId()))
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
---
+++ src/main/java/br/com/edgar/vitor/easforum/controller/AutenticacaoController.java
@@ -0,0 +1,43 @@
+package br.com.edgar.vitor.easforum.controller;
+
+import br.com.edgar.vitor.easforum.config.security.TokenService;
+import br.com.edgar.vitor.easforum.controller.dto.TokenDto;
+import br.com.edgar.vitor.easforum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+}
